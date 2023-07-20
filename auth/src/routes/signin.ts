afa93fd0b5a0afddcfc17f5e12e0ad87d306d3b1
ElianRivoira <eli_rivoira@hotmail.com>
2023-07-20 12:31:18 -0300

    common errors && middlewares library

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,10 +1,9 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
+import { validateRequest, BadRequestError } from '@er-tickets/common';
 
-import { validateRequest } from '../middlewares/validate-request';
 import { User } from '../models/user';
-import { BadRequestError } from '../errors/bad-request-error';
 import { Password } from '../services/password';
 
 const router = express.Router();
@@ -23,7 +22,7 @@ router.post(
     if (!existingUser) throw new BadRequestError('Invalid credentials');
 
     const passwordMatch = await Password.compare(existingUser.password, password);
-    if(!passwordMatch) throw new BadRequestError('Invalid credentials');
+    if (!passwordMatch) throw new BadRequestError('Invalid credentials');
 
     // Generate JWT
     const userJwt = jwt.sign(
