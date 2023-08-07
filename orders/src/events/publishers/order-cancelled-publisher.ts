70a9f404575db2ceab3730140a6225f178771ccd
Elian Rivoira <eli_rivoira@hotmail.com>
2023-08-07 16:22:50 -0300

    typo fix

--- orders/src/events/publishers/order-cancelled-publisher.ts
+++ orders/src/events/publishers/order-cancelled-publisher.ts
@@ -1,5 +1,5 @@
 import { Publisher, OrderCancelledEvent, Subjects } from '@er-tickets/common';
 
 export class OrderCancelledPublisher extends Publisher<OrderCancelledEvent> {
-  readonly subject = Subjects.OderCancelled;
+  readonly subject = Subjects.OrderCancelled;
 }
